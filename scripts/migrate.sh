30977a881bd4a4172f2b70e75fad1dffff813224
Vagner <vagner@users.noreply.github.com>
2026-03-06 00:07:59 +0000

    fix(local): corrige migracoes e remove version obsoleto do compose

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -3,6 +3,7 @@ set -eu
 
 : "${DATABASE_URL:?DATABASE_URL must be set}"
 
-for file in db/migrations/*_up.sql; do
-  sh ./scripts/psql.sh "$DATABASE_URL" -v ON_ERROR_STOP=1 -f "$file"
+for file in db/migrations/*.up.sql; do
+  [ -e "$file" ] || continue
+  sh ./scripts/psql.sh "$DATABASE_URL" -v ON_ERROR_STOP=1 < "$file"
 done
--- scripts/seed_csv.sh
+++ scripts/seed_csv.sh
@@ -3,4 +3,4 @@ set -eu
 
 : "${DATABASE_URL:?DATABASE_URL must be set}"
 
-sh ./scripts/psql.sh "$DATABASE_URL" -v ON_ERROR_STOP=1 -f scripts/import_csv.sql
+sh ./scripts/psql.sh "$DATABASE_URL" -v ON_ERROR_STOP=1 < scripts/import_csv.sql
